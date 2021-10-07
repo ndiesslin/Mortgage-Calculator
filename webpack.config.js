08999058c1f8281258c7cedcac0832ff9ecb1517
nicholas diesslin <nicholasdiesslin@gmail.com>
2021-10-07 14:10:45 -0500

    Added comments to adjust and updated webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, './'),
     },
     compress: true,
     port: 9000,
